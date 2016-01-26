24cea5fa486767fd349acd98e01b9f96b92dc418
Dieter Eickstädt <eickstaedt@deicon.de>
2016-01-26 19:55:55 +0100

    remove all

--- js/main.js
+++ js/main.js
@@ -9,12 +9,5 @@ $(window).scroll(function() {
     }
 });
 
-// On mobile, hide the avatar when expanding the navbar menu
-$('#main-navbar').on('show.bs.collapse', function () {
-  $(".navbar").addClass("top-nav-expanded");
-})
-$('#main-navbar').on('hidden.bs.collapse', function () {
-  $(".navbar").removeClass("top-nav-expanded");
-})
 
 // 2fc73a3a967e97599c9763d05e564189
