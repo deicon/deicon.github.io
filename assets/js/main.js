43997f37ca213bcc854b10ddaf8b2e01c7d80d03
Dieter Eickstaedt <eickstaedt@deicon.de>
2016-02-16 10:57:26 +0100

    show alert on form submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-// Dean Attali / Beautiful Jekyll 2015
-
-// Shorten the navbar after scrolling a little bit down
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar").addClass("top-nav-short");
-    } else {
-        $(".navbar").removeClass("top-nav-short");
-    }
-});
-
-
-// 2fc73a3a967e97599c9763d05e564189
+function success(message) {
+  alert(message)
+}
